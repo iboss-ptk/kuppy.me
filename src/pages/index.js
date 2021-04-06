04c163acad1887532ca004ae71b580201908aa64
Supanat Potiwarakorn <supanatpotiwarakorn@Supanats-MacBook-Pro.local>
2021-04-06 14:51:15 +0700

    add masonary

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,72 @@ import '@fontsource/montserrat'
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+const Card = ({ src, title }) => (
+  <div className="relative cursor-pointer">
+    <img src={src} />
+    <style jsx>{`
+      .overlay {
+        background: linear-gradient(0deg, black, transparent);
+      }
+    `}</style>
+    <div className="overlay absolute bottom-0 w-full h-24 px-4 pt-6">
+      {/* <div className="text-white text-lg">{title}</div> */}
+      {/* <div className="text-gray-400 text-sm">Photographer</div> */}
+    </div>
+  </div>
+);
+
+const images = [
+  {
+    title: "Photo title",
+    category: 'digital',
+    img:
+      "https://images.unsplash.com/photo-1536431311719-398b6704d4cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    title: "Photo title",
+    category: 'paper',
+    img:
+      "https://images.unsplash.com/photo-1510797215324-95aa89f43c33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    title: "Photo title",
+    img:
+      "https://images.unsplash.com/photo-1543297031-d102cd432d54?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    title: "Photo title",
+    category: 'digital',
+    img:
+      "https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    title: "Photo title",
+    img:
+      "https://images.unsplash.com/photo-1492724724894-7464c27d0ceb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    title: "Photo title",
+    img:
+      "https://images.unsplash.com/photo-1500534623283-312aade485b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    title: "Photo title",
+    img:
+      "https://images.unsplash.com/reserve/HgZuGu3gSD6db21T3lxm_San%20Zenone.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    title: "Photo title",
+    img:
+      "https://images.unsplash.com/photo-1444464666168-49d633b86797?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    title: "Photo title",
+    img:
+      "https://images.unsplash.com/photo-1465146344425-f00d5f5c8f07?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+  }
+];
+
 const IndexPage = () => {
   const [activeCategory, setActiveCategory] = useState('all')
 
@@ -47,6 +113,18 @@ const IndexPage = () => {
           <CategorySelector category='other' />
         </div>
 
+        <div className="masonry px-16 py-8">
+          {images
+          .filter(img => ({
+            'all': true,
+            'other': img.category == null
+          }[activeCategory] || activeCategory == img.category))
+          .map(item => (
+            <div className="overflow-hidden mb-4" key={item.img}>
+              <Card src={item.img} title={item.title} />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
